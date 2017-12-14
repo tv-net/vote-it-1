618cf9c44d300621fbf607ea4d013f3442098fce
Kailash rao <kailash.5@icloud.com>
2017-12-13 16:42:29 -0800

    Initial Commit

---
+++ vote it 1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vote it 1
+//
+//  Created by Kailash Rao on 12/13/17.
+//  Copyright © 2017 tv-net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ vote it 1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vote it 1
+//
+//  Created by Kailash Rao on 12/13/17.
+//  Copyright © 2017 tv-net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
